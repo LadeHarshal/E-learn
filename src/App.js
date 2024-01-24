4d00719a0f45d1bd6334079062a7fc546efe2d41
Harshal Lade <harshallade2@gmail.com>
2024-01-24 13:17:53 +0530

    images changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
 // function App() {
 //   return (
 //     <div className="App">
-     
+
 //       <Home/>
 //     </div>
 //   );
@@ -12,25 +12,44 @@
 
 // export default App;
 
-
-
-import React from 'react';
-import {  Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home';
-import Register from './Pages/Register.js'; // Import your Register component
-import Login from './Pages/Login.js';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Register from "./Pages/Register.js"; // Import your Register component
+import Login from "./Pages/Login.js";
+import StudentDashboard from "./Component/Student Dashboard/StudentDashboard.js";
+import StudentHome from "./Component/StudentHome/StudentHome.js";
+import TeacherDashboard from "./Component/Teacher/TeacherDashboard.js";
 
 function App() {
   return (
     <div className="App">
-    
       <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/register" element={<Register />} />
-  <Route path="/login" element={<Login />} />
-</Routes>
-
-     
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="/StudentDashboard"
+          element={<StudentDashboard />}
+        />
+        <Route
+          path="/StudentHome"
+          element={<StudentHome />}
+        />
+        <Route
+          path="/TeacherDashboard"
+          element={<TeacherDashboard />}
+        />
+      </Routes>
     </div>
   );
 }
