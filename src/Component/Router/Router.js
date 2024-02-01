bb382991fe390d7fb3c67daee5c5f3f7cef32249
Altamsh Pathan <98937760+Altamash1808@users.noreply.github.com>
2024-02-01 10:44:22 +0530

    msg

--- src/Component/Router/Router.js
+++ src/Component/Router/Router.js
@@ -3,6 +3,7 @@ import {Routes , Route} from 'react-router-dom';
 import Home from "../../Pages/Home";
 import Register from "../../Pages/Register";
 import Login from "../../Pages/Login";
+import CourseDetails from "../Course_Details/CourseDetails";
 
 
 
@@ -10,10 +11,12 @@ const Router = () => {
 
     return(
 <Routes>
+
   
 <Route path="/" element={<Home />} />
   <Route path="/register" element={<Register />} />
   <Route path="/login" element={<Login />} />
+        <Route path="/xyz" element={<CourseDetails/>}/>
 </Routes>
     )
     
