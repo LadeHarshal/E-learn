480ab61a8cde8a5bf8870f0c0a5db15023cedc4e
Mrsahil123 <techfinder111@gmail.com>
2024-03-20 11:01:48 +0530

    firebase setup for user creation

--- src/App.js
+++ src/App.js
@@ -16,8 +16,32 @@ import React from "react";
 import Layout from "./Component/Layout/Layout";
 import { Route, Routes } from "react-router-dom";
 import CourseDetails from "./Component/Course_Details/CourseDetails";
+import { app } from "./Firebase";
+//external for login and logout
+
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+
+const auth= getAuth(app);
 
 function App() {
+
+ const [user, setUser] = useState(null);
+
+  useEffect(()=>{
+
+    onAuthStateChanged(auth, user=> {
+      if(user){
+         // we are loged in
+         setUser(user);
+      } else{
+        // we are loged out
+        setUser(null);
+      }
+    });
+   },[]);
+
+if(user == null){
   return (
     <div className="App">
       <Layout>
@@ -26,6 +50,15 @@ function App() {
       </Routes>
       </Layout>
 
+    </div>
+
+  );
+}
+
+  return (
+    <div className="App">
+      <h1>Hello {user.email} </h1>
+      <button onClick={()=> signOut(auth)}>logout</button>
     </div>
 
   );
